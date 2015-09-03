fa143abb48a15a1bf99b1d1c17224ddbfdff429f
Roberto Cano <gabr1e11@hotmail.com>
2015-09-03 12:55:27 +0200

    Fixed when master is not checked out

--- publish.sh
+++ publish.sh
@@ -3,6 +3,12 @@ echo "\n[Building Jekyll site...]\n"
 bundle exec jekyll build
 
 echo "\n[Updating master...]\n"
+if [ ! -e master ]; then
+    git clone git@github.com:gabr1e11/gabr1e11.github.io.git master
+    cd master
+    git checkout master
+    cd ../
+fi
 cd master
 git pull
 cp -rf ../_site/* .
