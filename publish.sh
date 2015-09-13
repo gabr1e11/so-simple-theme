e5f2256c08ca40b4d439a2be6db6acd4b246f135
Roberto Cano <gabr1e11@hotmail.com>
2015-09-13 17:01:25 +0200

    Removed all Spanish entries

--- publish.sh
+++ publish.sh
@@ -2,16 +2,16 @@
 echo "\n[Building Jekyll site...]\n"
 bundle exec jekyll build
 
+SITE=`pwd`
+
 echo "\n[Updating master...]\n"
-if [ ! -e master ]; then
-    git clone git@github.com:gabr1e11/gabr1e11.github.io.git master
-    cd master
-    git checkout master
-    cd ../
-fi
-cd master
+cd /tmp/
+rm -rf robcgabriell
+git clone git@github.com:gabr1e11/robcgabriell.git
+cd robcgabriell
+git checkout master
 git pull
-cp -rf ../_site/* .
+cp -rf $SITE/_site/* .
 
 echo "\n[Committing and pushing...]\n"
 git add .
